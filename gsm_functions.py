8d29aa7963533a76b6f345f2deb8a26021e93cb8
Jorge Mira <jorge@gbyteinfo.com.br>
2023-11-14 19:25:22 -0300

    Ajustes no controle de inicialização do GSM(Tentativa de estabilziar o modulo e a conexzao com a operadora)

--- gsm_functions.py
+++ gsm_functions.py
@@ -6,39 +6,39 @@ class GsmSim800lMsg:
         TX_PIN = 17
         RX_PIN = 16
         self.GSM_Instance = UART(1, tx=TX_PIN, rx=RX_PIN, baudrate=0)  # baudrate definido como 9600
-        #self.numeros = ["+5519981728120"]
-        self.numeros = ["+5519992815223", "+5519974086936"]
+        #self.numeros = ["+5519992815223"]
+        self.numeros = ["+5519981728120", "+5519974086936"]
 
     def send_cmd(self, cmd, back=None, timeout=2000):
         print('Enviando:', cmd)
         self.GSM_Instance.write(cmd)
-        time.sleep_ms(500)
+        time.sleep_ms(300)
         wait_time = 0
         while wait_time < timeout:
             if self.GSM_Instance.any():
                 response = self.GSM_Instance.read()
                 print('Resposta:', response)
                 if back and back in response:
-                    time.sleep_ms(500)
+                    time.sleep_ms(300)
                     return True, response
-            time.sleep_ms(500)
+            time.sleep_ms(300)
             wait_time += 100
         return False, ''
 
     def enviar_sms(self, distancia):
-        def verifica_SMSC():
-            #self.send_cmd('AT+CSCA="+552191105300",145\r\n', 'OK')#claro
-            self.send_cmd('AT+CSCA="+551181136200",145\r\n', 'OK')#tim
-            time.sleep_ms(500)
-        verifica_SMSC()
-        
+        # Inicialização
         for numero in self.numeros:
-            # Inicialização
+            def verifica_SMSC():
+                self.send_cmd('AT+CSCA="+552191105300",145\r\n', 'OK')#claro
+                #self.send_cmd('AT+CSCA="+551181136200",145\r\n', 'OK')#tim
+                time.sleep_ms(500)
+            verifica_SMSC()
+
             success, _ = self.send_cmd('AT\r\n', 'OK')
             if not success:
                 print('Falha na inicialização.')
                 continue
-
+            
             # Configura o modo texto para SMS
             self.send_cmd('AT+CMGF=1\r\n', 'OK')
 
@@ -52,4 +52,3 @@ class GsmSim800lMsg:
                 print(f"Falha ao enviar SMS para {numero}")
 
         return "SMS enviado com sucesso"
-
